915d12638ace7ca5ca7211ef7654fed9dfa5dfb1
= <greggjs@miamioh.edu>
2014-01-19 04:06:13 +0000

    Added rounds api to settings and urls

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework_swagger',
     'rememerme.games.rest.games',
-    #'rememerme.games.rest.round'
+    'rememerme.games.rest.members',
+    'rememerme.games.rest.rounds'
 )
 
 REST_FRAMEWORK = {
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    #url(r'^rest/v1/games/(?P<game_id>[-\w]+)/round', include('rememerme.games.rest.round.urls')),
+    url(r'^rest/v1/games/(?P<game_id>[-\w]+)/rounds', include('rememerme.games.rest.round.urls')),
     url(r'^rest/v1/games/(?P<game_id>[-\w]+)/members', include('rememerme.games.rest.members.urls')),
     url(r'^rest/v1/games', include('rememerme.games.rest.games.urls')),
 )
