77559f03e51a465774dcd13352036af81c32b657
Andrew Oberlin <andyoberlin@gmail.com>
2014-01-27 15:04:47 -0500

    Removed the cards api since it belongs in the marketplace api.

--- rememerme/games/rest/cards/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-from rememerme.games.rest.games import views
-
-urlpatterns = patterns('',
-    url(r'^/(?P<card_id>[-\w]+)/?', views.CardSingleView.as_view()),
-)
